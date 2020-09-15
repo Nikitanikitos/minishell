5114f392cf564e34095dd16f3fc88712db3f33e4
Irving Micah <imicah@er-c1.kzn.21-school.ru>
2020-09-15 22:42:43 +0300

    added sort env_list

--- srcs/minishell/env_functions.c
+++ srcs/minishell/env_functions.c
@@ -49,21 +49,53 @@ void	change_or_add_value_env(char **key_value, t_list **env_list)
 	}
 }
 
+void	list_element_swap(t_list *first_element, t_list *second_element)
+{
+	void	*temp_content;
+
+	temp_content = first_element->content;
+	first_element->content = second_element->content;
+	second_element->content = temp_content;
+}
+
+void	add_env(t_list *env_list, t_env *env)
+{
+	t_list	*temp_elem;
+	t_env	*current_env;
+
+	while (env_list)
+	{
+		current_env = (t_env*)env_list->content;
+		if (ft_strcmp(env->key, current_env->key) < 0)
+		{
+			temp_elem = env_list->next;
+			env_list->next = ft_lstnew(env);
+			if (temp_elem)
+				env_list->next->next = temp_elem;
+			list_element_swap(env_list, env_list->next);
+			break;
+		}
+		else if (env_list->next == NULL)
+		{
+			env_list->next = ft_lstnew(env);
+			break ;
+		}
+		env_list = env_list->next;
+	}
+}
+
 t_list	*get_env_list(char **envp)
 {
 	t_env	*env;
-	t_list	*start_list;
 	t_list	*env_list;
 
 	env = env_init(ft_split(*envp++, '='));
 	env_list = ft_lstnew(env);
-	start_list = env_list;
 	while (*envp)
 	{
 		env = env_init(ft_split(*envp, '='));
-		env_list->next = ft_lstnew(env);
-		env_list = env_list->next;
+		add_env(env_list, env);
 		envp++;
 	}
-	return (start_list);
+	return (env_list);
 }
