7a85e0b16eecb873bf6bf8947ed79a63a33a89d0
Felucia Roxanne <froxanne@er-a2.kzn.21-school.ru>
2020-09-27 13:52:14 +0300

    repaired sighandlers

--- srcs/minishell/sighandler.c
+++ srcs/minishell/sighandler.c
@@ -1,23 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sigint_handler.c                                   :+:      :+:    :+:   */
+/*   sighandler.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: froxanne <froxanne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/16 15:42:11 by imicah            #+#    #+#             */
-/*   Updated: 2020/09/16 15:42:12 by imicah           ###   ########.fr       */
+/*   Updated: 2020/09/27 13:51:24 by froxanne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 void	sigint_handler(int signum)
-{
+{	
+	int		stat;
+	int		pid;
+
+	pid = waitpid(-1, &stat, WNOHANG);
 	if (signum == SIGINT)
 	{
 		write(1, "\n", 1);
-		type_prompt();
+		errno = 0;
+		if (pid)
+			type_prompt();
 	}
 }
 
