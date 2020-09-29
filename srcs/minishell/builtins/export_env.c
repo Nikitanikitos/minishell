2586bd043ced3b4db2ab389bb6ebcacf9f1f79ed
Irving Micah <imicah@er-b1.kzn.21-school.ru>
2020-09-29 13:33:32 +0300

    fixed export funcs

--- srcs/minishell/builtins/export_env.c
+++ srcs/minishell/builtins/export_env.c
@@ -23,8 +23,9 @@ void	print_export(t_list *env_list)
 		ft_putstr_fd(env.key, 1);
 		if (env.value)
 		{
-			write(1, "=", 1);
-			ft_putendl_fd(env.value, 1);
+			ft_putstr_fd("=\"", 1);
+			ft_putstr_fd(env.value, 1);
+			ft_putstr_fd("\"\n", 1);
 		}
 		else
 			write(1, "\n", 1);
@@ -37,16 +38,19 @@ int		valid_export_key(char *key)
 	if (ft_atoi(key))
 		return (0);
 	while (*key)
-		if (ft_isalnum(*key++) && *key != '_')
-			return (1);
-	return (0);
+	{
+		if (!ft_isalnum(*key) && *key != '_')
+			return (0);
+		key++;
+	}
+	return (1);
 }
 
 void	export_error(char **key_value)
 {
-	ft_putstr_fd("minishell: export: '", 2);
+	ft_putstr_fd("\e[31m\e[1mminishell: export: '", 2);
 	ft_putstr_fd(key_value[0], 2);
-	ft_putstr_fd("': not valid identifier\n", 2);
+	ft_putstr_fd("': not valid identifier\e[0m\n", 2);
 	free(key_value[0]);
 	if (key_value[1])
 		free(key_value[1]);
@@ -68,10 +72,10 @@ int		export(t_arguments *arguments, t_list *env_list)
 				export_error(key_value);
 				return (errno);
 			}
-			if (ft_str_double_len(key_value) == 1)
+			else if (ft_str_double_len(key_value) == 1)
 			{
 				if (ft_strchr(*arguments->argv, '='))
-					key_value[1] = ft_strdup("\'\'");
+					key_value[1] = ft_strdup("\0");
 			}
 			add_env(env_list, env_init(key_value));
 			free(key_value);
